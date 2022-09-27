fac02c5d1c68505ff48900805c2288eed9c04e0a
AbizerS10 <abizersusnerwala786@gmail.com>
2022-09-27 06:24:34 +0000

    first commit :)

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8081
+
+app.use(express.static('public'))
+app.listen(port, (req,res) => console.log(`listening on port ${port}!`))
+app.get('/',(req,res)=>{
+    res.send('API is running');
+    
+
+})
